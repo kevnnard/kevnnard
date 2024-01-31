3116dff50e0d5aca23c88e959b0f6bf495d2de00
KevnnardFedora <londonokevin9@gmail.com>
2024-01-31 16:31:14 -0500

    feat: add animate infinite scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,10 +70,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "scroll-x": {
+          "0%": { transform: "translateX(0px)" },
+          "100%": { transform: "translateX(calc(-160px * 7))" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "scroll-x": "scroll-x 40s linear infinite",
       },
     },
   },
