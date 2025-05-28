e87be0a01f77fc92600bbe435f8cb01afedcf1d3
Kevnnard MacOS <londonokevin9@gmail.com>
2025-05-28 13:16:17 -0500

    refactor(kevnnard): update  las vrsion Astro 5.1 and update dependencies to last versions , tailwincss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [mdx(), react()],
 });
